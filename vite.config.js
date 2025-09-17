b32bed15efe1bb43bfb5c0c68cd3d4c39665192d
Anastasia_S <sivokonanastasia1@gmail.com>
2025-09-18 02:30:39 +0300

    ain

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Project-group-16/", // ✅ добавил сюда
   plugins: [react()],
   build: {
     sourcemap: true,
-  }
+  },
 });
