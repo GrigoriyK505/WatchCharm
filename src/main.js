8e93b0af92d1c2938bf11468f281e1105a0513db
Anastasia_S <sivokonanastasia1@gmail.com>
2025-09-18 07:12:48 +0300

    ain

--- src/main.js
+++ src/main.js
@@ -2,17 +2,4 @@
 
 
 
-import './js/slider.js'
-
-document.querySelectorAll("load[src]").forEach(async el => {
-  const src = el.getAttribute("src");
-  if (src) {
-    try {
-      const res = await fetch(src);
-      const html = await res.text();
-      el.outerHTML = html; // заменяем <load> на содержимое файла
-    } catch (err) {
-      console.error(`Не удалось загрузить ${src}`, err);
-    }
-  }
-});
+import "./js/slider";
